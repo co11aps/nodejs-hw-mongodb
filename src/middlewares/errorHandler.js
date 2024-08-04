30339d5d2c634547e52989f1b7934113fcf074ef
co11aps <143988142+co11aps@users.noreply.github.com>
2024-08-04 20:31:41 +0800

    validation update

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -10,7 +10,7 @@ export const errorHandler = (error, req, res, next) => {
     return;
   }
   res.status(500).json({
-    message: 'Internal server error',
+    message: 'errorHandler: Internal server error',
     error: error.message,
   });
 };
--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,15 +1,38 @@
 import createHttpError from 'http-errors';
 
-export const validateBody = (schema) => async (req, res, next) => {
-  try {
-    await schema.validateAsync(req.body, {
-      abortEarly: false,
-    });
-    next();
-  } catch (err) {
-    const error = createHttpError(400, 'Bad Request', {
-      errors: err.details,
-    });
-    next(error);
-  }
-};
+// export const validateBody = (schema) => async (req, res, next) => {
+//   try {
+//     await schema.validateAsync(req.body, {
+//       abortEarly: false,
+//     });
+//     console.log('validateBody works');
+//     next();
+//   } catch (err) {
+//     const error = createHttpError(400, 'Bad Request', {
+//       errors: err.details,
+//     });
+//     next(error);
+//   }
+// };
+
+// import createHttpError from 'http-errors';
+
+export function validateBody(schema) {
+  return async (req, res, next) => {
+    try {
+      await schema.validateAsync(req.body, { abortEarly: false });
+
+      next();
+    } catch (error) {
+      console.log({ message: error.message });
+      console.log({ details: error.details });
+
+      next(
+        createHttpError(
+          400,
+          error.details.map((err) => err.message).join(', '),
+        ),
+      );
+    }
+  };
+}
