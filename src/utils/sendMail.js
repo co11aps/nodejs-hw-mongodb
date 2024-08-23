0244d5c536cc5ca28a3d3ed1d59cdb73794d52fa
co11aps <143988142+co11aps@users.noreply.github.com>
2024-08-23 23:56:29 +0800

    Update sendMail.js

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,6 +1,5 @@
 import nodemailer from 'nodemailer';
 import { SMTP } from '../constants/index.js';
-import { env } from '../utils/env.js';
 
 const transporter = nodemailer.createTransport({
   host: SMTP.SMTP_HOST,
