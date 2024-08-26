84be8b4735b69a29a54774c907a997ef4d28b61c
co11aps <143988142+co11aps@users.noreply.github.com>
2024-08-26 17:55:38 +0800

    swagger

--- src/constants/index.js
+++ src/constants/index.js
@@ -27,3 +27,5 @@ export const CLOUDINARY = {
   API_KEY: process.env.API_KEY,
   API_SECRET: process.env.API_SECRET,
 };
+
+export const SWAGGER_PATH = path.resolve('docs', 'swagger.json');
--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -28,6 +29,8 @@ export function setupServer() {
   // );
 
   app.use(express.json());
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use(cors());
 
